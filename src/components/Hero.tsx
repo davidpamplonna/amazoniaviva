c2c1883671c5c44660bea82172024e7595418760
David Pamplona <davypamp@gmail.com>
2025-04-05 15:26:23 -0300

    finalizando layout

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,23 +3,23 @@ import { LuArrowRight } from "react-icons/lu";
 export function Hero() {
 
     return (
-        <section className="relative min-h-screen">
+        <section className="relative min-h-[580px] md:min-h-[780px] ">
             <div 
-            className="absolute inset-0 bg-contain bg-no-repeat md:bg-cover bg-center"
-            style={{ backgroundImage: 'url("https://plus.unsplash.com/premium_photo-1687428554388-395f7f690ddb?q=80&w=2085&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")' }}>
-                <div className="absolute inset-0 bg-black opacity-40"></div>
+            className="absolute inset-0 bg-no-repeat bg-cover bg-center-center md:bg-center"
+            style={{ backgroundImage: 'url("./../public/banner.png")' }}>
+                <div className="absolute inset-0 bg-black opacity-45"></div>
             </div>
             <div className="relative max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pt-32 pb-16">
-                <div className="max-w-3xl">
+                <div className="max-w-3xl mt-10 md:mt-25">
                     <h1 className="text-4xl md:text-6xl font-bold text-white mb-6">Protegendo a Amazônia e seus povos</h1>
                     <p className="text-lg md:text-2xl text-white/90 mb-8">Junte-se a nós na luta pela preservação da maior floresta tropical do mundo e pelos direitos das comunidades que a habitam</p>
                 </div>
                 <div className="flex flex-col sm:flex-row gap-4">
-                    <button className="bg-[#018e5c] text-white px-8 py-3 rounded-md hover:bg-[#016d46] flex items-center">
+                    <button className="bg-[#018e5c] text-white px-8 py-3 rounded-md hover:bg-[#016d46] flex items-center justify-center">
                     Doe Agora
                         <LuArrowRight className="ml-2 mt-1" size={20} />
                     </button>
-                    <button className="border-2 border-white text-white text-white px-8 py-3 rounded-md hover:bg-white/10 transition-colors">
+                    <button className="border-2 border-white text-white px-8 py-3 rounded-md hover:bg-white/10 transition-colors">
                         Seja Voluntário
                     </button>
                 </div>
--- src/components/NewsSection.tsx
+++ src/components/NewsSection.tsx
@@ -38,13 +38,13 @@ export function NewsSection() {
         <section className="py-16 bg-white">
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                 <div className="flex items-center justify-between mb-12">
-                    <div className="max-w-2xl">
-                        <h2 className="text-xl md:text-4xl font-bold text-gray-900 mb-2">Últimas Notícias</h2>
+                    <div className="max-w-2xl w-[207px] md:w-full">
+                        <h2 className="text-2xl md:text-4xl font-bold text-gray-900 mb-2">Últimas Notícias</h2>
                         <p className="text-gray-600 text-sm md:text-base">Acompanhe as últimas atualizações sobre nossa causa</p>
                     </div>
-                    <a href="#" className="bg-[#018e5c]/10 text-[#018e5c] px-6 py-3  rounded-full hover:bg-[#018e5c]/20 transition-colors flex item-center font-medium duration-400 ">
+                    <a href="#" className="text-sm items-center md:text-base bg-[#018e5c]/10 text-[#018e5c] px-6 py-3  rounded-full hover:bg-[#018e5c]/20 transition-colors flex font-medium  duration-400 ">
                         Ver todas
-                        <TiArrowRight className="ml-2" size={26} />
+                        <TiArrowRight className="ml-1 md: md:mt-1" size={26} />
                         </a>
                     
                 </div>
